e1241680ea697e04caaec174dcbbee0dc44002f1
Hanjise0ng <tjdwl3269@gmail.com>
2025-02-20 16:17:56 +0900

    Chore : 잘못 올라간 Product 파일 삭제

--- backend/src/main/java/com/example/coffee/product/controller/ProductController.java
+++
@@ -1,4 +0,0 @@
-package com.example.coffee.product.controller;
-
-public class ProductController {
-}
--- backend/src/main/java/com/example/coffee/product/service/ProductService.java
+++
@@ -1,4 +0,0 @@
-package com.example.coffee.product.service;
-
-public class ProductService {
-}
