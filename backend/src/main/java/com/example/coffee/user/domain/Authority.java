070003a709da6c84d4a8a2bf82a2f034bddaad04
kces0822 <kces0822@gmail.com>
2025-02-21 22:48:23 +0900

    Feat: Authority ENUM 생성

---
+++ backend/src/main/java/com/example/coffee/user/domain/Authority.java
@@ -0,0 +1,6 @@
+package com.example.coffee.user.domain;
+
+public enum Authority {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- backend/src/main/java/com/example/coffee/user/domain/User.java
+++ backend/src/main/java/com/example/coffee/user/domain/User.java
@@ -19,11 +19,9 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-//    @Column(nullable = false)
-//    private String name;
-//
-//    @Column(nullable = false)
-//    private String password;
+    @Column(nullable = false)
+    private String password;
+
 //
 //    public void update(CreateUserRequest request) {
 //        name = request.name();
