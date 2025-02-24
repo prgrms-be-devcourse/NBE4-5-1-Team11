b7d44802bc4b83c0132ba62c31badf6ea80d7fe1
Hweepadam <160405935+huipadyam@users.noreply.github.com>
2025-02-25 01:21:37 +0900

    Docs: Swagger로 API 명세서 생성 (#26)
    * Docs: Swagger로 API 명세서 생성
    
    * Docs: Swagger로 API 명세서 생성

--- backend/src/main/java/com/example/coffee/order/controller/OrderController.java
+++ backend/src/main/java/com/example/coffee/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.example.coffee.order.controller;
 import com.example.coffee.order.controller.dto.CreateOrderRequest;
 import com.example.coffee.order.controller.dto.OrderResponse;
 import com.example.coffee.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
+@Tag(name = "Order API", description = "주문 관련 API")
 public class OrderController {
 
     private final OrderService orderService;
 
     // 주문 결제 동시에 유저 저장, 주문 저장
+    @Operation(summary = "주문 생성")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrderResponse create(@RequestBody CreateOrderRequest orderRequest) {
@@ -24,6 +28,7 @@ public class OrderController {
     }
 
     // 유저 id로 주문 조회
+    @Operation(summary = "주문 목록 조회")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<OrderResponse> findAll() {
@@ -31,6 +36,7 @@ public class OrderController {
     }
 
     // 유저 이메일로 주문 조회
+    @Operation(summary = "주문 단건 조회 - userEmail")
     @GetMapping("/email")
     @ResponseStatus(HttpStatus.OK)
     public List<OrderResponse> findAllByEmail(@RequestParam String email) {
@@ -38,6 +44,7 @@ public class OrderController {
     }
 
     // 주문 id로 주문 단건 조회
+    @Operation(summary = "주문 단건 조회 - userId")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public OrderResponse findById(@PathVariable Long id) {
@@ -45,6 +52,7 @@ public class OrderController {
     }
 
     // 주문 id로 주문 단건 삭제
+    @Operation(summary = "주문 단건 삭제")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
--- backend/src/main/java/com/example/coffee/order/controller/OrderProductController.java
+++ backend/src/main/java/com/example/coffee/order/controller/OrderProductController.java
@@ -3,6 +3,8 @@ package com.example.coffee.order.controller;
 
 import com.example.coffee.order.controller.dto.OrderProductResponse;
 import com.example.coffee.order.service.OrderProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/order-product")
 @RequiredArgsConstructor
+@Tag(name = "Order-Product API", description = "주문-상품 관련 API")
 public class OrderProductController {
 
     private final OrderProductService orderProductService;
 
     // 상품 전체 조회
+    @Operation(summary = "상품 목록 조회")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<OrderProductResponse> findAll() {
@@ -24,6 +28,7 @@ public class OrderProductController {
     }
 
     // 상품 단건 삭제
+    @Operation(summary = "상품 삭제")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
