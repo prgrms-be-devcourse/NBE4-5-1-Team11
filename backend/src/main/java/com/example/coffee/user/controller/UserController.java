eca5373a65325c4781f34b47b2890995adfb4a78
kces0822 <kces0822@gmail.com>
2025-02-25 00:20:53 +0900

    Docs: Swagger로 API 명세서 생성

--- backend/src/main/java/com/example/coffee/user/controller/UserController.java
+++ backend/src/main/java/com/example/coffee/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.coffee.order.controller.dto.OrderResponse;
 import com.example.coffee.user.controller.dto.CreateUserRequest;
 import com.example.coffee.user.controller.dto.CreateUserResponse;
 import com.example.coffee.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,22 +15,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Tag(name = "User API", description = "회원 관련 API")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원 가입")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CreateUserResponse createUser(@RequestBody CreateUserRequest request) {
         return userService.saveUser(request);
     }
-
+    
+    @Operation(summary = "회원 단건 조회")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CreateUserResponse getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
+    @Operation(summary = "회원 목록 조회")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<CreateUserResponse> getAllUsers(){
@@ -41,6 +47,7 @@ public class UserController {
 //        return userService.updateUser(id, request);
 //    }
 
+    @Operation(summary = "회원 삭제")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Long id){
@@ -48,6 +55,7 @@ public class UserController {
     }
 
    // 해당 유저의 주문 정보 모두 조회
+   @Operation(summary = "특정 회원 주문 목록 조회")
     @GetMapping("/{id}/orders")
     @ResponseStatus(HttpStatus.OK)
     public List<OrderResponse> getUserOrders(@PathVariable Long id){
