9ba2757dac58b68d0ae34978f3dbdfb063ce4e64
Hanjise0ng <tjdwl3269@gmail.com>
2025-02-25 14:43:57 +0900

    Chore: swagger Tag 추가

--- backend/src/main/java/com/example/coffee/order/controller/AdminOrderController.java
+++ backend/src/main/java/com/example/coffee/order/controller/AdminOrderController.java
@@ -3,6 +3,7 @@ package com.example.coffee.order.controller;
 import com.example.coffee.order.controller.dto.OrderResponse;
 import com.example.coffee.order.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/orders")
 @RequiredArgsConstructor
+@Tag(name = "Order API", description = "주문 관련 API")
 public class AdminOrderController {
 
     private final OrderService orderService;
