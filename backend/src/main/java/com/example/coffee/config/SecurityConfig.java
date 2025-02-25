b365adb3b66039445d84c1fc88c85f7b0228610f
Hanjise0ng <tjdwl3269@gmail.com>
2025-02-25 12:06:35 +0900

    Chore: config 설정 주석 처리

--- backend/src/main/java/com/example/coffee/config/SecurityConfig.java
+++ backend/src/main/java/com/example/coffee/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.example.coffee.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -34,12 +32,12 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                        .requestMatchers("/users/register", "/users/login", "/users/refresh").permitAll() // 회원가입, 로그인, 토큰 갱신은 허용
-                        .requestMatchers(HttpMethod.POST, "/orders").permitAll() // 주문은 전체 허용
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/users/list").hasRole("ADMIN") // 🔒 `/users/**` 엔드포인트는 인증 필요
-                        .anyRequest().authenticated() // 다른 모든 요청도 인증 필요
+//                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+//                        .requestMatchers("/users/register", "/users/login", "/users/refresh").permitAll() // 회원가입, 로그인, 토큰 갱신은 허용
+//                        .requestMatchers(HttpMethod.POST, "/orders").permitAll() // 주문은 전체 허용
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .requestMatchers("/users/list").hasRole("ADMIN") // 🔒 `/users/**` 엔드포인트는 인증 필요
+                        .anyRequest().permitAll() // 다른 모든 요청도 인증 필요
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
 
