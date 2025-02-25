436348b712fce1852e867282c58c08d24f27a68d
강웅빈 <kob05314@gmail.com>
2025-02-25 11:01:48 +0900

    Feat: 배송 처리 스케줄링 (#34)
    * Bug : 로그인된 유저를 가져오지 못하는 버그 픽스
    
    * Feat : 배송 처리 스케줄링
    
    * Chore : 트랜잭션 어노테이션 import 수정

--- backend/src/main/java/com/example/coffee/security/SecurityUtil.java
+++ backend/src/main/java/com/example/coffee/security/SecurityUtil.java
@@ -7,47 +7,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.Optional;
 
 public class SecurityUtil {
-    // 로그인된 상황에서 현재 유저
-    /*public static User getCurrentUserWithLogin() {
-        try {
-            return getUser();
-        } catch (ClassCastException e) {
-            throw new IllegalArgumentException("User not Found");
-        }
-    }
-
-    private static User getUser() {
-        Object principal = SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getPrincipal();
-
-        if (principal instanceof String) {
-            throw new IllegalArgumentException("User not Found");
-        }
-
-        AuthDetails authDetails = (AuthDetails) principal;
-
-        return authDetails.getUser();
-    }*/
-
-    // 로그인 여부를 모르는 상황에서 현재 유저 찾기 -> Optional User
-    /*public static Optional<User> getOptUser() {
-        return Optional.ofNullable(getCurrentUserOrNotLogin());
-    }
-
-    private static User getCurrentUserOrNotLogin() {
-        try {
-            return getUser();
-        } catch (Exception e) {
-            return null;
-        }
-    }*/
 
     // 로그인된 상황에서 현재 유저 반환 (예외 발생 시 "User not Found")
     public static User getCurrentUserWithLogin() {
         try {
             return getUser();
-        } catch (Exception e) {
+        } catch (ClassCastException e) {
             throw new IllegalArgumentException("User not Found");
         }
     }
