98ed1c8833a674346c88c2e32131476fb5deb832
Woongbin06 <kob05314@gmail.com>
2025-02-25 10:35:25 +0900

    Bug : 로그인된 유저를 가져오지 못하는 버그 픽스

--- backend/src/main/java/com/example/coffee/security/AuthDetails.java
+++ backend/src/main/java/com/example/coffee/security/AuthDetails.java
@@ -32,4 +32,24 @@ public class AuthDetails implements UserDetails {
     public String getUsername() {
         return user.getEmail();
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
